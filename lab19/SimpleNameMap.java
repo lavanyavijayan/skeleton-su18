6890736113ce7584bfd2867e0c945ac826f77ab3
Lavanya <lvijayan1@berkeley.edu>
2018-07-19 14:40:33 -0700

    added SimpleNameMap

--- lab19/SimpleNameMap.java
+++ lab19/SimpleNameMap.java
@@ -1,38 +1,93 @@
+import java.util.LinkedList;
+import java.lang.reflect.Array;
+
 public class SimpleNameMap {
 
-    /* Instance variables here? */
+    /* Instance variables here*/
+    private int size;
+    private LinkedList<Entry>[] entries;
 
-    /* TODO: Add a constructor here */
+    /* Constructs an empty SimpleNameMap with initial capacity of 26. */
+    @SuppressWarnings("unchecked")
+    public SimpleNameMap() {
+        entries = (LinkedList<Entry> []) new LinkedList[26];
+    }
 
     /* Returns true if the given KEY is a valid name that starts with A - Z. */
     private static boolean isValidName(String key) {
         return 'A' <= key.charAt(0) && key.charAt(0) <= 'Z';
     }
 
+    /* Returns a specific integer when given a key. */
+    private int hash(String key) {
+        return (int) (key.charAt(0) - 'A');
+    }
+
     /* Returns true if the map contains the KEY. */
     boolean containsKey(String key) {
-        // TODO: YOUR CODE HERE
+        int keyHash = hash(key);
+        LinkedList<Entry> entryList = entries[keyHash];
+        for (Entry e : entryList) {
+            if (e.key.equals(key)) {
+                return true;
+            }
+        }
         return false;
     }
 
     /* Returns the value for the specified KEY. If KEY is not found, return
        null. */
     String get(String key) {
-        // TODO: YOUR CODE HERE
-        return null;
+        if (containsKey(key)) {
+            LinkedList<Entry> entryList = entries[hash(key)];
+            String val = "";
+            for (Entry e : entryList) {
+                if (e.key.equals(key)) {
+                    val = e.value;
+                    break;
+                }
+            }
+            return val;
+        } else {
+            return null;
+        }
     }
 
     /* Puts a (KEY, VALUE) pair into this map. If the KEY already exists in the
        SimpleNameMap, replace the current corresponding value with VALUE. */
     void put(String key, String value) {
-        // TODO: YOUR CODE HERE
+        if (containsKey(key)) {
+            LinkedList<Entry> entryList = entries[hash(key)];
+            for (Entry e : entryList) {
+                if (e.key.equals(key)) {
+                    e.value = value;
+                    break;
+                }
+            }
+        } else {
+            if (isValidName(key)) {
+                entries[hash(key)].add(new Entry(key, value));
+                size += 1;
+            }
+        }
     }
 
     /* Removes a single entry, KEY, from this table and return the VALUE if
        successful or NULL otherwise. */
     String remove(String key) {
-        // TODO: YOUR CODE HERE
-        return null;
+        if (containsKey(key)) {
+            String val = get(key);
+            LinkedList<Entry> entryList = entries[hash(key)];
+            for (Entry e : entryList) {
+                if (e.key.equals(key)) {
+                    e = null;
+                    break;
+                }
+            }
+            return val;
+        } else {
+            return null;
+        }
     }
 
     private static class Entry {
