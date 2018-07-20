0f13020e5bf0ba250da31451a285da043ed26acb
Lavanya <lvijayan1@berkeley.edu>
2018-07-19 22:38:01 -0700

    adding Map61BL

--- lab19/Map61BL.java
+++ lab19/Map61BL.java
@@ -5,17 +5,17 @@ public interface Map61BL<K, V> extends Iterable<K> {
     /* Removes all of the mappings from this map. */
     void clear();
 
-    /* Returns true if this map contains a mapping for the specified key KEY. */
+    /* Returns true if this map contains a mapping for the specified key (KEY). */
     boolean containsKey(K key);
 
-    /* Returns the value to which the specified key KEY is mapped, or null i
+    /* Returns the value to which the specified key (KEY) is mapped, or null if
        this map contains no mapping for KEY. */
     V get(K key);
 
     /* Puts the specified key-value pair (KEY, VALUE) in this map. */
     void put(K key, V value);
 
-    /* Removes and returns a key KEY and its associated value. */
+    /* Removes and returns a key (KEY)'s associated value. */
     V remove(K key);
 
     /* Removes a particular key-value pair (KEY, VALUE) and returns true if
